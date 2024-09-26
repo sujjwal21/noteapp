c8a1ea100d928119c6c97eafb4393ae6f01a3fed
sujjwal21 <ujjwal@adveniotecnosys.tech>
2024-09-26 15:57:57 +0530

    css

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,8 +6,8 @@ interface TodoListProps {
 
 const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
   return (
-    <div className="w-full h-80 overflow-y-auto max-w-lg mx-auto p-5 border border-gray-300 rounded-lg bg-white-100 shadow-md scrollbar-thin ">
-    <h2 className="text-2xl font-semibold text-center mb-4 text-gray-700 border-b-2">Notes</h2>
+    <div className="w-full h-80 overflow-y-auto max-w-lg mx-auto p-5 bg-white-100 scrollbar-thin ">
+    <h2 className="text-2xl flex font-semibold text-center items-start mb-4 text-gray-700 border-b-2">Notes</h2>
     <ul className="list-none p-0">
       {tasks.length > 0 ? (
         tasks.map((task, index) => (
